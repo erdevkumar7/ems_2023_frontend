42d39c0c0a45e9ea4e113b135e668f0e0cab455f
devendrabarodiya <barodiyadevendra7@gmail.com>
2023-11-08 01:08:30 +0530

    create compose for sendmail

--- src/app/sendmail/page.tsx
+++ src/app/sendmail/page.tsx
@@ -1,118 +1,137 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
-import { TextField, Button, Grid, Paper } from "@mui/material";
+import { TextField, Button, Grid, Paper, Box } from "@mui/material";
 import { handleSendEmail } from "@/app/services/emailServices";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const ComposeEmail = () => {
-    const [recipient, setRecipient] = useState("");
-    const [cc, setCC] = useState("");
-    const [bcc, setBCC] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
-    const [senderEmail, setSenderEmail] = useState("");
-    const [attachments, setAttachments] = useState([]);
+  const [recipient, setRecipient] = useState("");
+  const [cc, setCC] = useState("");
+  const [bcc, setBCC] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [senderEmail, setSenderEmail] = useState("");
+  const [attachments, setAttachments] = useState([]);
+  const [isCCOpen, setIsCCOpen] = useState(false);
+  const [isBCCOpen, setIsBCCOpen] = useState(false);
 
+  const toggleCC = () => {
+    setIsCCOpen(!isCCOpen);
+  };
 
-    useEffect(() => {
-        let localData: any;
-        if (typeof window !== "undefined") {
-          localData = window.localStorage.getItem("userData");
-        }
-        const userData = JSON.parse(localData);
-        setSenderEmail(userData.email)
-    },[])
+  const toggleBCC = () => {
+    setIsBCCOpen(!isBCCOpen);
+  };
 
-    const handleRecipientChange = (e: any) => {
-        setRecipient(e.target.value);
-    };
+  useEffect(() => {
+    let localData: any;
+    if (typeof window !== "undefined") {
+      localData = window.localStorage.getItem("userData");
+    }
+    const userData = JSON.parse(localData);
+    setSenderEmail(userData.email);
+  }, []);
 
-    const handleCCChange = (e: any) => {
-        setCC(e.target.value);
-    };
+  const handleRecipientChange = (e: any) => {
+    setRecipient(e.target.value);
+  };
 
-    const handleBCCChange = (e: any) => {
-        setBCC(e.target.value);
-    };
+  const handleCCChange = (e: any) => {
+    setCC(e.target.value);
+  };
 
-    const handleSubjectChange = (e: any) => {
-        setSubject(e.target.value);
-    };
+  const handleBCCChange = (e: any) => {
+    setBCC(e.target.value);
+  };
 
-    const handleMessageChange = (e: any) => {
-        setMessage(e.target.value);
-    };
+  const handleSubjectChange = (e: any) => {
+    setSubject(e.target.value);
+  };
 
-    // const handleAttachmentChange = (e: any) => {
-    //     const files = e.target.files;
-    //     const fileArray = Array.from(files);
-    //     setAttachments(fileArray);
-    // };
+  const handleAttachmentChange = (e: any) => {
+    const files = e.target.files;
+    const fileArray: any = Array.from(files);
+    setAttachments(fileArray);
+  };
 
-    const handleSendClick = async () => {
-        console.log(recipient, subject, message)
-        const mailData = await handleSendEmail({
-            user_id: 2,
-            to_email: recipient,
-            subject: subject,
-            cc: cc,
-            bcc: bcc,
-            content: message,
-            sender_email_id: senderEmail,
-        })
+  const handleSendClick = async () => {
+    console.log(recipient, subject, message);
+    const mailData = await handleSendEmail({
+      user_id: 2,
+      to_email: recipient,
+      subject: subject,
+      cc: cc,
+      bcc: bcc,
+      content: message,
+      sender_email_id: senderEmail,
+    });
 
-        console.log(senderEmail,'tttt',mailData)
+    console.log(senderEmail, "tttt", mailData);
+  };
 
-    };
+  return (
+    <Paper
+      elevation={3}
+      style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}
+    >
+      <h2>Compose Email</h2>
+      <form>
+        <Box display="flex" alignItems="center" marginBottom={2}>
+          <TextField
+            label="To"
+            fullWidth
+            variant="outlined"
+            value={recipient}
+            onChange={handleRecipientChange}
+          />
+          <Button onClick={toggleCC} style={{ marginLeft: "10px" }}>
+            CC
+          </Button>
+          {isCCOpen && (
+            <TextField
+              label="CC"
+              fullWidth
+              variant="outlined"
+              value={cc}
+              onChange={handleCCChange}
+            />
+          )}
+          <Button onClick={toggleBCC} style={{ marginLeft: "10px" }}>
+            BCC
+          </Button>
+          {isBCCOpen && (
+            <TextField
+              label="BCC"
+              fullWidth
+              variant="outlined"
+              value={bcc}
+              onChange={handleBCCChange}
+            />
+          )}
+        </Box>
+        <Box marginBottom={2}>
+          <TextField
+            label="Subject"
+            fullWidth
+            variant="outlined"
+            value={subject}
+            onChange={handleSubjectChange}
+          />
+        </Box>
+        <Box>
+          <ReactQuill theme="snow" value={message} onChange={setMessage} />
+        </Box>
 
-    return (
-        <Paper elevation={3} style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}>
-            <h2>Compose Email</h2>
-            <form>
-                <TextField
-                    label="To"
-                    fullWidth
-                    variant="outlined"
-                    value={recipient}
-                    onChange={handleRecipientChange}
-                />
-                <TextField
-                    label="CC"
-                    fullWidth
-                    variant="outlined"
-                    value={cc}
-                    onChange={handleCCChange}
-                />
-                <TextField
-                    label="BCC"
-                    fullWidth
-                    variant="outlined"
-                    value={bcc}
-                    onChange={handleBCCChange}
-                />
-                <TextField
-                    label="Subject"
-                    fullWidth
-                    variant="outlined"
-                    value={subject}
-                    onChange={handleSubjectChange}
-                />
-                <TextField
-                    label="Message"
-                    multiline
-                    fullWidth
-                    variant="outlined"
-                    value={message}
-                    onChange={handleMessageChange}
-                />
-                {/* <input type="file" multiple onChange={handleAttachmentChange} /> */}
-                <Grid container justifyContent="flex-end">
-                    <Button variant="contained" color="primary" onClick={handleSendClick}>
-                        Send
-                    </Button>
-                </Grid>
-            </form>
-        </Paper>
-    );
+        <input type="file" multiple onChange={handleAttachmentChange} />
+        <Grid container justifyContent="flex-end">
+          <Button variant="contained" color="primary" onClick={handleSendClick}>
+            Send
+          </Button>
+        </Grid>
+      </form>
+    </Paper>
+  );
 };
 
 export default ComposeEmail;
